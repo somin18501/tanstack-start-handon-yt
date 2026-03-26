71c06ea5c8e34a6fde5931c0887e9fd13f0b39e9
sgandhi-ontic <sgandhi@ontic.co>
2026-03-26 22:56:50 +0530

    [PATCH] update better auth url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,5 +2,6 @@ import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
+  baseURL:
+    'https://tanstack-start-handon-c29797vmn-somin18501s-projects.vercel.app',
 })
